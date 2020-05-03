8dfd1675a46b40c259aace07179a5fed3dec5927
urvashigupta7 <urvashi072000@gmail.com>
2020-05-03 10:29:30 +0530

    binary tree add

--- src/binaryTree/BinaryTree.java
+++ src/binaryTree/BinaryTree.java
@@ -1,6 +1,9 @@
 package binaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class BinaryTree {
     public class Node{
@@ -105,5 +108,139 @@ public class BinaryTree {
         }else{
             return search(data,node.right);
         }
+    }
+    public int height(){
+        return height(this.root);
+    }
+    private  int height(Node node){
+        if(node==null){
+            return 0;
+        }
+        int leftHeight=height(node.left);
+        int rightHeight=height(node.right);
+        return Math.max(leftHeight,rightHeight)+1;
+    }
+    public void mirror(){
+        mirror(this.root);
+    }
+    private void mirror(Node node){
+       if(node==null){
+           return;
+       }
+       Node temp=node.left;
+       node.left=node.right;
+       node.right=temp;
+       mirror(node.left);
+       mirror(node.right);
+    }
+    public void preorder(){
+        preorder(this.root);
+    }
+    private void preorder(Node node){
+        if(node==null){
+            return;
+        }
+        System.out.print(node.data+" ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+    public void inorder(){
+        inorder(this.root);
+    }
+    private void inorder(Node node){
+        if(node==null){
+            return;
+        }
+        inorder(node.left);
+        System.out.print(node.data+" ");
+        inorder(node.right);
+    }
+    public void postorder(){
+        postorder(this.root);
+    }
+    private void postorder(Node node){
+        if(node==null){
+            return;
+        }
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.data+" ");
+    }
+    public void levelorder(){
+        Queue <Node>q=new LinkedList<>();
+        q.offer(this.root);
+        while(!q.isEmpty()){
+            Node node=q.poll();
+            System.out.print(node.data+" ");
+            if(node.left!=null){
+                q.offer(node.left);
+            }
+            if(node.right!=null){
+                q.offer(node.right);
+            }
+        }
+        System.out.println();
+    }
+    public void preorderI(){
+        Stack<Node>st=new Stack<>();
+        st.push(this.root);
+        while(!st.isEmpty()){
+            Node node=st.pop();
+            System.out.print(node.data+" ");
+            if(node.right!=null){
+                st.push(node.right);
+            }
+            if(node.left!=null){
+                st.push(node.left);
+            }
+        }
+        System.out.println();
+    }
+    public void inorderI(){
+        Stack<Node>st=new Stack<>();
+        Node curr=this.root;
+        while(curr!=null || !st.isEmpty()){
+            while(curr!=null){
+                st.push(curr);
+                curr=curr.left;
+            }
+            curr=st.pop();
+            System.out.print(curr.data+" ");
+            curr=curr.right;
+        }
+        System.out.println();
+    }
+    public int diameter(){
+        return diameter(this.root);
+    }
+    private int diameter(Node node){
+        if(node==null){
+            return 0;
+        }
+        int f1=height(node.left)+height(node.right)+1;
+        int f2=diameter(node.left);
+        int f3=diameter(node.right);
+        return  Math.max(f1,Math.max(f2,f3));
+    }
+    public class DiaPair{
+        int height;
+        int diameter;
+    }
+    public int diameterBtr(){
+        return diameterBtr(this.root).diameter;
+    }
+    private DiaPair diameterBtr(Node node){
+        if(node==null){
+            DiaPair d=new DiaPair();
+            d.height=0;
+            d.diameter=0;
+            return d;
+        }
+        DiaPair left=diameterBtr(node.left);
+        DiaPair right=diameterBtr(node.right);
+        DiaPair mr=new DiaPair();
+        mr.height=Math.max(left.height,right.height)+1;
+        mr.diameter=Math.max(left.height+ right.height+1,Math.max(left.diameter,right.diameter));
+        return mr;
     }
 }
--- src/binaryTree/Client.java
+++ src/binaryTree/Client.java
@@ -8,6 +8,18 @@ public class Client {
         System.out.println(bt.size2());
         System.out.println(bt.max());
         System.out.println(bt.search(100));
+        System.out.println(bt.height());
+        bt.preorder();
+        System.out.println();
+        bt.inorder();
+        System.out.println();
+        bt.postorder();
+        System.out.println();
+        bt.levelorder();
+        bt.preorderI();
+        bt.inorderI();
+        System.out.println(bt.diameter());
+        System.out.println(bt.diameterBtr());
 
     }
 }
